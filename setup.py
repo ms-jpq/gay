61349534fc25f4da5ec4bb15edea227ef0368bf4
Fly me to the moon <git@bigly.dog>
2020-06-30 22:25:46 -0700

    setuptools

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+LONG_DESCRIPTION = """
+"""
+
+setup(
+    name="gay",
+    version="1.0",
+    description="",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="",
+    author_email="",
+    url="",
+    scripts=["gay"],
+)
