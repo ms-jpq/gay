309afeddc7ec52bc8569e283b2d558fd96729262
Fly me to the moon <git@bigly.dog>
2020-07-01 01:20:53 -0700

    print all colours

--- demo.sh
+++ demo.sh
@@ -7,23 +7,36 @@ set -o pipefail
 cd "$(dirname "$0")" || exit 1
 
 
-if [[ $# -gt 0 ]]
-then
-  ./gay -f -l
-  printf '\n\n'
-  ./gay -f -g
-  printf '\n\n'
-  ./gay -f -b
-  printf '\n\n'
-  ./gay -f -t
-  printf '\n\n'
-  ./gay -f -a
-  printf '\n\n'
-  ./gay -f -p
-  printf '\n\n'
-  ./gay -f -n
-  printf '\n\n'
-  ./gay -f --gq
-else
-  ./gay < ./gay
-fi
+./gay -f -l
+printf '\n\n'
+./gay -f -g
+printf '\n\n'
+./gay -f -b
+printf '\n\n'
+./gay -f -t
+printf '\n\n'
+./gay -f -a
+printf '\n\n'
+./gay -f -p
+printf '\n\n'
+./gay -f -n
+printf '\n\n'
+./gay -f --gq
+
+
+TEXT="$(< /dev/stdin)"
+./gay -l <<< "$TEXT"
+printf '\n\n'
+./gay -g <<< "$TEXT"
+printf '\n\n'
+./gay -b <<< "$TEXT"
+printf '\n\n'
+./gay -t <<< "$TEXT"
+printf '\n\n'
+./gay -a <<< "$TEXT"
+printf '\n\n'
+./gay -p <<< "$TEXT"
+printf '\n\n'
+./gay -n <<< "$TEXT"
+printf '\n\n'
+./gay --gq <<< "$TEXT"
